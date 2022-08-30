969a217f0c0f792a7a93d9922ccc7bac9907f966
i-atanasov <ivayloatanasov@zohomail.eu>
2022-08-30 17:53:41 +0300

    ssr react with api

--- ssr-tasklist/src/components/App.js
+++ ssr-tasklist/src/components/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
 
-const App = (props) => {
-  return (
-    <div>App</div>
-  )
+import RenderTasks from './RenderTasks';
+
+
+
+class App extends React.Component {
+  state = { tasks: [] };
+  
+  componentDidMount() {
+    fetch('https://api.jsonbin.io/v3/b/6304b4b65c146d63ca7bd581', { 
+        headers: { 
+          "Content-Type":	"application/json",
+          "X-Access-Key": "6304b5fda1610e63860b814f"
+        }
+    }).then((res) => res.json())
+    .then(tasksList => {
+        console.log(tasksList.record.tasks)
+        this.setState({ tasks: tasksList.record.tasks });
+    });
+  
+  }
+  render() {
+    return (
+      <div>
+          <h1>FT Onboarding</h1> 
+          <RenderTasks tasks={this.state.tasks}/>     
+        </div>
+      )
+    }
 }
 
 export default App;
---
+++ ssr-tasklist/src/components/RenderTasks.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+const RenderTasks = (props) => {
+    const tasks = props.tasks;
+    const renderList = () => {
+        return tasks.map(currentTask=> {
+         let done = currentTask.completed ? 'lightgreen' : 'tomato';
+      
+        return (
+            <div key={currentTask.id} className="listItem" style={{ "borderRight": `5px solid ${done}` }}>
+                <div className="">
+                    <div className="">
+                        {currentTask.type}
+                    </div> 
+                    <div className="">
+                        Topic {currentTask.topic}  <br/>
+                        Apr. duration: {currentTask.duration}<br/>
+                    </div>
+                    <div className="">
+                        Resourses: To be rendered  <br/>
+                    </div>
+                    <div className="">
+                        Goal: {currentTask.results}  <br/>
+                    </div>
+                    <br/>
+                    <hr></hr>
+                    </div>
+                </div>
+        );
+    });
+};
+
+return (
+      <div>{renderList()}</div>
+    )
+}
+
+export default RenderTasks;
