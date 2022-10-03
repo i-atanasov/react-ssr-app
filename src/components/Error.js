0c8ac4837fe39c12678bf270b23633513741b31c
i-atanasov <ivayloatanasov@zohomail.eu>
2022-10-03 11:51:32 +0300

    heroku headers edit

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default Error = (props) => {
   return (
-      <div class="ui active dimmer">
-        <div class="ui text loader">
-          <div>{props.message}</div>
-        </div>
-      </div>    
+    <div class="ui active dimmer">
+      <div class="ui text loader">
+        <div>{props.message}</div>
+      </div>
+    </div>
   )
 }
